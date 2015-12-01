69a027dbe82d049f09fb0e09415f832c236bd75e
@hbk <nut.rainbowpuke@gmail.com>
2015-12-01 20:04:30 +0700

    Reformat code

--- app/src/main/java/com/popcorn/MainActivity.java
+++ app/src/main/java/com/popcorn/MainActivity.java
@@ -3,13 +3,9 @@ package com.popcorn;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.res.Configuration;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
@@ -17,41 +13,42 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
     private String[] titles;
     private ListView drawerList;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
 
-
-
-
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//Code to run when an item in the navigation drawer gets clicked
+            //Code to run when an item in the navigation drawer gets clicked
             selectItem(position);
         }
-    };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView)findViewById(R.id.drawer);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//Populate the ListView
-        drawerList.setAdapter(new ArrayAdapter<String>(this,
+
+        drawerList.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_activated_1, titles));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
         if (savedInstanceState == null) {
             selectItem(0);
         }
 
-        //Create the ActionBarDrawerToggle
+        // Create the ActionBarDrawerToggle
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
                 R.string.open_drawer, R.string.close_drawer) {
+
             //Called when a drawer has settled in a completely closed state
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
@@ -63,19 +60,19 @@ public class MainActivity extends ActionBarActivity {
                 invalidateOptionsMenu();
             }
         };
-        drawerLayout.setDrawerListener(drawerToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
 
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
     private void selectItem(int position) {
-// update the main content by replacing fragments
+        // Update the main content by replacing fragments
         Fragment fragment;
         switch(position) {
             case 1:
-                fragment = new NewMovie();
+                fragment = new NewReviewFragment();
                 break;
             case 2:
                 fragment = new Friends();
@@ -86,14 +83,17 @@ public class MainActivity extends ActionBarActivity {
             default:
                 fragment = new RecommendList();
         }
+
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.content_frame, fragment);
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         ft.commit();
-        //Set the action bar title
+
+        // Set the action bar title
         setActionBarTitle(position);
-        //Close drawer
+
+        // Close drawer
         drawerLayout.closeDrawer(drawerList);
     }
 
@@ -106,11 +106,13 @@ public class MainActivity extends ActionBarActivity {
         }
         getSupportActionBar().setTitle(title);
     }
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerToggle.syncState();
     }
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -125,8 +127,7 @@ public class MainActivity extends ActionBarActivity {
         else{
             return super.onOptionsItemSelected(item);
         }
-        }
-
+    }
 
 }
 
