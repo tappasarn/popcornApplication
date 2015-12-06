9fdd9fd97c70ab1c1ad1b567fba7278c06d7d40c
@hbk <nut.rainbowpuke@gmail.com>
2015-12-06 17:27:07 +0700

    Add configuration keys for sharedPreferences.

--- app/src/main/java/com/popcorn/config/Configurations.java
+++ app/src/main/java/com/popcorn/config/Configurations.java
@@ -3,9 +3,21 @@ package com.popcorn.config;
 
 public class Configurations {
 
+    public static final String SHARED_PREF_KEY = "com.popcorn.sharedPreferences";
+
+    // ------------------ User information --------------------------------------
+    public static final String USER_TOKEN = "user.token";
+    public static final String USER_READABLE_ID = "user.readable_id";
+    public static final String USER_EMAIL = "user.email";
+    public static final String USER_PROFILE_PIC_URL = "user.profile_pic_url";
+
+
+    // ------------------ APIs --------------------------------------------------
+
     public static final String API_BASE_URL = "http://10.0.3.2:3000";
 
     // Users related endpoints
     public static final String LOGIN_URL = API_BASE_URL + "/users/login";
+    public static final String SIGNUP_URL = API_BASE_URL + "/users/create";
 
 }
