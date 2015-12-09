75cc26673ee19bc3a3aa4451c6393eb6fd936f2c
@hbk <nut.rainbowpuke@gmail.com>
2015-12-09 22:03:11 +0700

    Implement push notification

---
+++ app/src/main/java/com/popcorn/gcm/GcmPreferences.java
@@ -0,0 +1,8 @@
+package com.popcorn.gcm;
+
+public class GcmPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
