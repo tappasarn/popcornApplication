dfd8e2339be0f689d34394c478abe333ccf8df04
@hbk <nut.rainbowpuke@gmail.com>
2015-12-10 16:05:45 +0700

    Add image icon to drawer list.

---
+++ app/src/main/java/com/popcorn/DrawerAdapter.java
@@ -0,0 +1,55 @@
+package com.popcorn;
+
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class DrawerAdapter extends BaseAdapter {
+
+    private Context context;
+    private SimpleDrawerItem[] items;
+    private LayoutInflater inflator;
+
+    public DrawerAdapter(Context context, SimpleDrawerItem[] items) {
+        this.context = context;
+        this.items = items;
+        this.inflator = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = View.inflate(context, R.layout.drawer_list, null);
+
+        ImageView iconImage = (ImageView) view.findViewById(R.id.drawerListIcon);
+        TextView menuText = (TextView) view.findViewById(R.id.drawerListText);
+
+        iconImage.setImageDrawable(ContextCompat.getDrawable(context, items[position].getIconResId()));
+        menuText.setText(items[position].getText());
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/popcorn/SimpleDrawerItem.java
@@ -0,0 +1,28 @@
+package com.popcorn;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SimpleDrawerItem {
+
+    private int iconResId;
+    private String text;
+
+    public SimpleDrawerItem(int iconResId, String text) {
+        this.iconResId = iconResId;
+        this.text = text;
+    }
+
+    public int getIconResId() {
+        return iconResId;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
